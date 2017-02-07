1d745393224bea60210c863e5bf35b6c40a51e71
jrsahuquillo <jrsahuquillo@gmail.com>
2017-02-07 03:09:27 +0100

    Production fixes OK

---
+++ test.js
@@ -0,0 +1,19 @@
+// "use strict"
+//
+// const assert = require('assert')
+// const express = require('express')
+// const request = require('supertest')
+// const api = require('./routes')
+// const app = require('./routes/index.js')
+//
+// const req = request("http://localhost:3001")
+//
+// describe('GET /log/list', function() {
+//  it('respond with json', function(done) {
+//    request(api)
+//      .get('/log/list')
+//      .set('Accept', 'application/json')
+//      .expect('Content-Type', /json/)
+//      .expect(200, done)
+//  })
+// })
