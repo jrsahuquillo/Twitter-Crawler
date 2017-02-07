290f3a8a758038830b09706149e456fc9b873f02
jrsahuquillo <jrsahuquillo@gmail.com>
2017-02-07 02:13:09 +0100

    DB deploying fixes

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var MONGODB_URI ="mongodb://username:password@ds139899.mlab.com:39899/influencity"
 
-
 module.exports = {
   port: process.env.PORT || 3001,
   db: process.env.MONGODB_URI || 'mongodb://localhost:27017/log'
